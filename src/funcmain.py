e8511163833f6cf49063eb77114c4a22c0f347a6
rahul-08-11 <grahulkumar4444@gmail.com>
2025-01-28 10:04:40 +0530

    handle accent in province

--- src/funcmain.py
+++ src/funcmain.py
@@ -67,8 +67,8 @@ class TransportOrders:
                 Drop_off_Location=body.get("Dropoff_Location", ""),
                 PickupLocation=body.get("Pickup_Location", ""),
                 special_instructon=body.get("Special_Instruction", ""),
-                Tax_Province=body.get("Pickup_Province", ""),
-                Dropoff_Province=body.get("Dropoff_Province", ""),
+                Tax_Province=body.get("Pickup_Province", "").replace("é", "e"),
+                Dropoff_Province=body.get("Dropoff_Province", "").replace("é", "e"),
                 Pickup_City = body.get("Pickup_City", ""),
                 Dropoff_City= body.get("Dropoff_City", ""),
             )
